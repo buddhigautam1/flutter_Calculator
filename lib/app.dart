825f315f12d6c3816b3b5a6ce398b4ee649f3c64
buddhi <buddhigautam421@gmail.com>
2024-04-20 13:40:22 +0530

    initial

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: Container(
+        color: Colors.grey,
+      ),
+    );
+  }
+}
